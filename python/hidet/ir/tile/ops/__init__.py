48b1f489d0414a9043b8800316f21ab32cbdb525
Yaoyao Ding <dingyaoyao.cs@gmail.com>
2023-08-18 03:23:43 -0400

    .

--- python/hidet/ir/tile/ops/__init__.py
+++ python/hidet/ir/tile/ops/__init__.py
@@ -1,15 +1,15 @@
 from .creation import arange, full
 from .memory import load, store
 from .system import num_programs, program_id
-from .transform import broadcast, reshape, expand_dims
+from .transform import broadcast, expand_dims
 from .convert_layout import convert_layout
-from .reduce import ReduceOp
+from .reduce import sum, min, max
 from .debug import debug_print
 
 from .creation import Arange, Full
 from .memory import Load, Store
-from .transform import Broadcast, Reshape, ExpandDims
+from .transform import Broadcast, ExpandDims
 from .convert_layout import ConvertLayout
 from .arthimatic import UnaryTileOp, BinaryTileOp
-from .reduce import sum, min, max
+from .reduce import ReduceOp
 from .debug import DebugPrint
